6a773fa437963ce253c6a5088d3f713a86ca1cb8
ThinhNguyen2692 <nndthinh2802@gmail.com>
2022-10-22 15:31:42 +0700

    update phu 1.3

--- wordpress/wp-content/themes/twentytwenty/template-parts/content.php
+++ wordpress/wp-content/themes/twentytwenty/template-parts/content.php
@@ -31,19 +31,7 @@ if (!is_single()) {
 			get_template_part('template-parts/entry-header');
 
 ?>
-			<?php
-			if ( is_search() || ! is_singular() && 'summary' === get_theme_mod( 'blog_content', 'full' ) ) {
-				the_excerpt();
-			} else {
-				the_content( __( 'Continue reading', 'twentytwenty' ) );
-			}	
-			if (!is_search()) {
-				get_template_part('template-parts/featured-image');
-
-			}
-
-			?>
-
+		
 			<div class="post-inner <?php echo is_page_template('templates/template-full-width.php') ? '' : 'thin'; ?> ">
 
 				<div class="entry-content">
