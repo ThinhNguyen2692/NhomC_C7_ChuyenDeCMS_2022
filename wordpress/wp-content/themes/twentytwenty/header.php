63e039d878b5c4a029566436749a85955309fc20
MSI\voquo_706 <voquochuy30112@gmail.com>
2022-10-28 22:31:19 +0700

    update css

--- wordpress/wp-content/themes/twentytwenty/header.php
+++ wordpress/wp-content/themes/twentytwenty/header.php
@@ -20,10 +20,10 @@
     <link rel="profile" href="https://gmpg.org/xfn/11">
     <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/content.css" type="text/css" media="screen" />
 
-    <!-- module1 CSS -->
-    <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/header1.css" type="text/css" media="screen" />
+
     <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/project.css" type="text/css" media="screen" />
-    <!-- Dropdown -->
+
+	
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
